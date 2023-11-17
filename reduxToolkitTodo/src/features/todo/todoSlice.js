202f1720dd4adf5f8487bab18543648c5f24d163
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2023-11-17 21:36:49 +0530

    redux toolkit

--- reduxToolkitTodo/src/features/todo/todoSlice.js
+++ reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -4,4 +4,23 @@ const initialState = {
     todos: [{id: 1, text: "Hello World"}]
 }
 
-export const todoSlice = createSlice({})
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState,
+    reducers: {
+        addTodo: (state, action) => {
+            const todo = {
+                id: nanoid(),
+                text: action.payload
+            }
+            state.todos.push(todo)
+        },
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload )
+        }
+    }
+})
+
+export const {addTodo, removeTodo} = todoSlice.actions 
+
+export default todoSlice.reducer
